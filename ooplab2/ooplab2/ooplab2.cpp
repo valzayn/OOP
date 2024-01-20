37ac4da9fbfb11c4b5137b66998c311204c66cbe
valzayn <76012455+valzayn@users.noreply.github.com>
2024-01-20 16:34:53 +1000

    Add files via upload

---
+++ ooplab2/ooplab2/ooplab2.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+int main()
+{
+    int rows;       
+    int columns; 
+    int s, k;
+    std::cin >> rows >> columns;
+    int** numbers = new int* [rows] {};
+    int* arr_s = new int[columns] {};
+    for (int i{}; i < rows; i++)
+    {
+        numbers[i] = new int[columns] {};
+    }
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            std::cin >> numbers[i][j];
+        }
+    }
+  
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            std::cout << numbers[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        s = 0;
+        for (int j = 0; j < columns; j++)
+        {
+            s += numbers[j][i];
+        }
+        arr_s[i] = s;
+    }
+    for (int i = 0; i < rows; i++) {
+        std::cout << arr_s[i] << " ";
+    }
+    k = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (arr_s[i] - numbers[i][j] < numbers[i][j]) {
+                k++;
+            }
+        }
+        std::cout << std::endl;
+    }
+    std::cout << k << "\n";
+}
